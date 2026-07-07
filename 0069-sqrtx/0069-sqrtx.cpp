39c4e247c39b523376c8b59ed9486f5b79c4b546
coderstop244 <152056461+coderstop244@users.noreply.github.com>
2026-07-07 22:47:59 +0530

    Time: 0 ms (100%), Space: 8.6 MB (49.56%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,30 +1,17 @@
 class Solution {
-private:
-int BinarySearch(int n){
-    int s=0;
-    int e=n;
-   
-    int ans = -1;
-    
-    while(s<=e){
-     long long int mid = s+(e - s)/2;
-     long long int square = mid*mid;
-    if(square==n){
-        return mid;
-    }
-    if(square>n){
-        e = mid - 1;
-    }else{
-        ans = mid;
-        s = mid+1;
-        
-  }
-}
-  return ans;
- 
-}
 public:
     int mySqrt(int x) {
-     return BinarySearch(x);
+        int low = 1;
+        int high = x;
+        int ans = 1;
+        while(low<=high){
+            int long long mid = low +(high - low)/2;
+            if(1LL*mid*mid<=x){
+                low = mid+1;
+            }else{
+                high = mid-1;
+            }
+        }
+        return high;
     }
 };
