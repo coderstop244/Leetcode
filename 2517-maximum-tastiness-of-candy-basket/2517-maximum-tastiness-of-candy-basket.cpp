425b49d8e828dee499570d03d71ed2b77666c703
coderstop244 <152056461+coderstop244@users.noreply.github.com>
2026-07-20 23:16:47 +0530

    Time: 46 ms (66.22%), Space: 52.1 MB (99.89%) - LeetHub

--- 2517-maximum-tastiness-of-candy-basket/2517-maximum-tastiness-of-candy-basket.cpp
+++ 2517-maximum-tastiness-of-candy-basket/2517-maximum-tastiness-of-candy-basket.cpp
@@ -1,37 +1,33 @@
 class Solution {
 private:
-bool canAchive(int mid,vector<int>& price, int k){
-    int count  = 1;
-    int last = price[0];
-
-    for(int i = 1 ;i<price.size();i++){
-        if(price[i] - last>=mid){
-            count++;
-            last = price[i];
+    bool canAchive(int mid,vector<int>& price, int k){
+        int count=1;
+        int last = price[0];
+        for(int i = 1;i<price.size();i++){
+                if((price[i] - last) >=mid){
+                    count++;
+                    last = price[i];
+                }
         }
-    } 
-    return count>=k;
-
-}
+        return count >=k;
+    }
 public:
     int maximumTastiness(vector<int>& price, int k) {
-        //1st step :- sort the prices
+        //Sort the elements
 
         sort(price.begin(),price.end());
 
-        //2nd step
         int s = 0;
         int e = price.back() - price.front();
         int ans = 0;
-
         while(s<=e){
         int mid = s+(e-s)/2;
-        if(canAchive(mid,price,k)){
-            ans = mid;
-            s = mid +1;
-        }else{
-            e = mid - 1;
-        }
+            if(canAchive(mid,price,k)){
+                ans = mid;
+                s = mid +1;
+            }else{
+                e = mid-1;
+            }
         }
         return ans;
     }
